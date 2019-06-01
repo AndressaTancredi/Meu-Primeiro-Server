697d9212398f7531235d39d52016c17a7c144bbf
Andressa Tancredi <andressa.tancredi@hotmail.com>
2019-06-01 19:48:32 -0300

    Confirgurando o controller

--- ComidasController.js
+++ ComidasController.js
@@ -1,35 +1,28 @@
-const comidas = {pratosFavoritos: [
-    {
-      nome: "Batata frita",
-      descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
-      imagem: "img/Batata-frita.jpg"
-    },
-    {
-      nome: "Macarronada",
-      descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
-      imagem: "img/macarronada.jpg"
-    },
-    {
-      nome: "Falafel",
-      descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
-      imagem: "img/falafel.jpg"
-    },
-    {
-      nome: "Creme de abóbora",
-      descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
-      imagem: "img/creme-de-abobora.jpg"
-    },
-  ]}
+const comidas = {pratosFavoritos: []
+}
 
 const getAll = () => {
-    return comidas
+  return pratos
 }
 
 const add = (comida) => {
-    comidas.pratosFavoritos.push(comida)
-}
-module.exports = {
-    getAll,
-    add
+  comida.id = Math.random().toString(36).substr(-8)
+  getAll().pratosFavoritos.push(comida)
 }
 
+const remove = (id) => {
+
+  let comidasRestantes = getAll().pratosFavoritos
+
+  comidasRestantes.filter((comida) =>{
+    return comida !== id
+
+  })
+}
+
+module.exports = {
+  getAll,
+  add,
+  remove,
+  pratos
+}
--- server-wonderful-master/server.js
+++ server-wonderful-master/server.js
@@ -1,22 +1,23 @@
-const express = require('express') //Sempre passa ele como string
-/* Outra opção do ES6:
-import express from 'express' */
-
+const express = require('express') 
 const cors = require('cors') 
 const bodyParser = require('body-parser')
 const controller = require('../ComidasController')
+const servidor = express() 
 
-const servidor = express() //Meu servidor usa o express
-servidor.use(cors()) //Meu servidor usa o CORS 
+servidor.use(cors()) 
 
 servidor.get("/comidas", (request, response)=>{
   response.send(controller.getAll())
 })
-//Usa o bodyParser para quando chegar no body já estiver parseado.
-servidor.post('./comidas', bodyParser.json(), (request,response) => {
-  
+
+servidor.post("/comidas", bodyParser.json(), (request, response) => {
   controller.add(request.body)
   response.send(201)
+})
+
+servidor.delete('/comidas/:id',(request,response) =>{
+  controller.remove(request.params.id)
+  response.sendStatus(204)
 })
 
 servidor.listen(3000);
